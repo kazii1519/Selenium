c0e25095d72a7143423695346186957f8cbcad9b
izdehaar <ezdehaarkazi215@gmail.com>
2024-10-10 02:33:37 +0530

    done

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+package exp07;
+
+import org.openqa.selenium.WebDriver;
+
+public class App {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://github.com/kaziiqra");
+    }
+}
